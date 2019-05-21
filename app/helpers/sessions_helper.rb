4f1f2b2028afcf3dc569337a43e562bfcd910c53
Hafiz Waqar <waqar.novatore@gmail.com>
2019-05-21 15:56:03 +0500

    Add password reset

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -35,11 +35,7 @@ module SessionsHelper
   end
 
   # Logs out the current user.
-  def log_out
-    forget(current_user)
-    session.delete(:user_id)
-    @current_user = nil
-  end
+
   def forget(user)
     user.forget
     cookies.delete(:user_id)

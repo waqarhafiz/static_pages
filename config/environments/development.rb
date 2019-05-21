00929c499af207acefd6a259c1fa3bd7475c45cc
Hafiz Waqar <waqar.novatore@gmail.com>
2019-05-21 11:42:32 +0500

    Change in production file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   gitlab_rails['smtp_user_name'] = "waqar.novatore@gmail.com"
   gitlab_rails['smtp_password'] = "ecjzbajjtaxeoftp"
   gitlab_rails['smtp_domain'] = "smtp.gmail.com"
-  gitlab_rails['smtp_authentication'] = "login"
+  gitlab_rails['smtp_authentication'] = "plain"
   gitlab_rails['smtp_enable_starttls_auto'] = true
   gitlab_rails['smtp_tls'] = false
   gitlab_rails['smtp_openssl_verify_mode'] = 'peer'
